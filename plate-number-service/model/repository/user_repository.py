c3472224259be0ba9dd5a6ac5b85b9cefb889107
thuhang2510 <tunguyet2510@gmail.com>
2023-04-07 15:08:18 +0700

    add try-except in class user_repository

--- plate-number-service/model/repository/user_repository.py
+++ plate-number-service/model/repository/user_repository.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm.session import Session
-from model.database import SessionLocal, get_db
+import logging
+from model.database import SessionLocal
 from model.models import DbUser
+
+logger = logging.getLogger()
 
 class UserRepository():
 
@@ -8,21 +10,37 @@ class UserRepository():
         self.db = SessionLocal()
 
     def get_all_users(self):
-        return self.db.query(DbUser).all()
+        try:
+            return self.db.query(DbUser).all()
+        except Exception as e:
+            logger.exception(e)
+            return None
     
     def create(self, data):
-        self.db.add(data)
-        self.db.commit()
-        self.db.refresh(data)
-        
-        return data
+        try:
+            self.db.add(data)
+            self.db.commit()
+            self.db.refresh(data)
+            
+            return data
+        except Exception as e:
+            logger.exception(e)
+            return None
     
     def get_by_id(self, id):
-        return self.db.get(DbUser, id)
+        try:
+            return self.db.get(DbUser, id)
+        except Exception as e:
+            logger.exception(e)
+            return None
     
     def update(self, id, filter):
-        data = self.db.query(DbUser).filter_by(id = id)
-        count = data.update(filter)
-        self.db.commit()
+        try:
+            data = self.db.query(DbUser).filter_by(id = id)
+            count = data.update(filter)
+            self.db.commit()
 
-        return count
+            return count
+        except Exception as e:
+            logger.exception(e)
+            return None
