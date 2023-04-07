bc8437e4b95dd5130a67c250bad46e16e5dc8d3e
thuhang2510 <tunguyet2510@gmail.com>
2023-04-07 14:35:31 +0700

    create method CRU

--- plate-number-service/model/implementation/user_implement.py
+++ plate-number-service/model/implementation/user_implement.py
@@ -1,5 +1,4 @@
-
-
+from model.models import DbUser
 from model.repository.user_repository import UserRepository
 
 
@@ -10,3 +9,13 @@ class UserImplement():
     
     def get_all_user(self):
         return UserRepository().get_all_users()
+    
+    def create(self, data):
+        return UserRepository().create(DbUser(**data))
+    
+    def get_by_id(self, id):
+        return UserRepository().get_by_id(id)
+    
+    def update(self, id):
+        return UserRepository().update(id, filter)
+    
