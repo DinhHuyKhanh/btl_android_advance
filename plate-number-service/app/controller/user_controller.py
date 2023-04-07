604015fc5f73b4539d9f96fd2781f1067f038f2e
thuhang2510 <tunguyet2510@gmail.com>
2023-04-07 14:43:15 +0700

    create api for get, create, delete

--- plate-number-service/app/controller/user_controller.py
+++ plate-number-service/app/controller/user_controller.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter, Response, status
-from enum import Enum
-from typing import Optional
-from app.service.user_service import UserService
+from fastapi import APIRouter
+from schemas import UserSchema
 from app.settings import user_service
 from app.settings import user_model
 
@@ -10,8 +8,19 @@ router = APIRouter(
     tags=['users']
 )
 
-@router.get(
-  '/',
-  )
+@router.get('/')
 def get_all_user():
     return user_service.get_all_user(user_model)
+
+@router.get('/{id}')
+def get_user_by_id(id: int):
+    return {'data': user_service.get_by_id(id, user_model)}
+
+@router.post('')
+async def create(user: UserSchema):
+    user=user.dict()
+    return {'data': user_service.create(user, user_model)}
+
+@router.delete('/{id}')
+async def delete(id: int):
+    return {'data': user_service.delete(id, user_model)}
