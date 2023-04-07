f9bdba07f4dc0679f6d53e5dd760de19c0169bcc
thuhang2510 <tunguyet2510@gmail.com>
2023-04-07 14:39:46 +0700

    add method create, delete, get_by_id

--- plate-number-service/app/service/user_service.py
+++ plate-number-service/app/service/user_service.py
@@ -4,3 +4,13 @@ class UserService():
 
     def get_all_user(self, model):
         return model.get_all_user()
+    
+    def create(self, data, model):
+        return model.create(data)
+    
+    def delete(self, id, model):
+        filter = {"activate": False}
+        return model.update(id, filter)
+    
+    def get_by_id(self, id, model):
+        return model.get_by_id(id)
