fbdb1df5c8d4f74626e36b57ade8dd75065d54e6
thuhang2510 <tunguyet2510@gmail.com>
2023-04-07 14:31:39 +0700

    create class DbUser represents the table user_data

--- plate-number-service/model/models.py
+++ plate-number-service/model/models.py
@@ -1,11 +1,17 @@
 from sqlalchemy.sql.sqltypes import Integer, String
 from model.database import Base
-from sqlalchemy import Column
+from sqlalchemy import DECIMAL, Boolean, Column
 
 
 class DbUser(Base):
-  __tablename__ = 'users'
+  __tablename__ = 'user_data'
   id = Column(Integer, primary_key=True, index=True)
-  username = Column(String)
+  fullName = Column(String)
+  phoneNumber = Column(String)
   email = Column(String)
   password = Column(String)
+  personalNumber = Column(String)
+  address = Column(String)
+  activate = Column(Boolean)
+  coin = Column(DECIMAL)
+  birthday =  Column(String)
