9d6a09303bfe7c022997f56b899fc94d2903aab7
thuhang2510 <tunguyet2510@gmail.com>
2023-04-07 14:33:27 +0700

    add method CRUD to CRUD user with db

--- plate-number-service/model/repository/user_repository.py
+++ plate-number-service/model/repository/user_repository.py
@@ -9,3 +9,20 @@ class UserRepository():
 
     def get_all_users(self):
         return self.db.query(DbUser).all()
+    
+    def create(self, data):
+        self.db.add(data)
+        self.db.commit()
+        self.db.refresh(data)
+        
+        return data
+    
+    def get_by_id(self, id):
+        return self.db.get(DbUser, id)
+    
+    def update(self, id, filter):
+        data = self.db.query(DbUser).filter_by(id = id)
+        count = data.update(filter)
+        self.db.commit()
+
+        return count
