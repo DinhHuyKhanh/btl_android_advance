25b880d41f263b029869ded57a7aa1d4a6c89ea7
thuhang2510 <tunguyet2510@gmail.com>
2023-04-08 11:42:38 +0700

    add connection config of email

--- plate-number-service/conf.py
+++ plate-number-service/conf.py
@@ -1,19 +1,28 @@
 from dotenv import load_dotenv
 import os
 
+from fastapi_mail import ConnectionConfig
+
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 #DATABASE 
-DATABASE = {
-    'drivername': 'mysql+mysqlconnector',
-    'username': os.getenv('DB_USERNAME'),
-    'password': os.getenv('DB_PASSWORD'),
-    'host':  os.getenv('DB_HOST'),
-    'port':  os.getenv('DB_PORT'),
-    'database':  os.getenv('DB_DATABASE')
-}
+DATABASE = f"mysql+mysqlconnector://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
 
 #MEDIA
 STATIC_MEDIA = os.getenv('STATIC_MEDIA')
 SAVE_MODEL = os.getenv('SAVE_MODEL')
+
+#EMAIL
+EMAIL = ConnectionConfig(
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
+    MAIL_FROM = os.getenv('MAIL_FROM'),
+    MAIL_PORT = int(os.getenv('MAIL_PORT')),
+    MAIL_SERVER = os.getenv('MAIL_SERVER'),
+    MAIL_FROM_NAME = os.getenv('MAIL_FROM_NAME'),
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = False,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True
+)
