988b5e9fa5afe5918e204cb5be74abb8693e3457
thuhang2510 <tunguyet2510@gmail.com>
2023-04-08 11:42:06 +0700

    add email_router in api router

--- plate-number-service/app/router.py
+++ plate-number-service/app/router.py
@@ -3,6 +3,7 @@ from app.controller.blog_get import router as blog_get_router
 from app.controller.blog_post import router as blog_post_router
 from app.controller.user_controller import router as user_router
 from app.controller.detect_plate_controller import router as plate_router
+from app.controller.email_controller import router as email_router
 
 api_router = APIRouter(
     prefix='/api/v1'
@@ -12,3 +13,4 @@ api_router.include_router(blog_get_router)
 api_router.include_router(blog_post_router)
 api_router.include_router(user_router)
 api_router.include_router(plate_router)
+api_router.include_router(email_router)
