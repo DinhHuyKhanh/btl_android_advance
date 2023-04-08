5ecc1aacd3edbaa01177f6ab29c2c037e1b24fa7
thuhang2510 <tunguyet2510@gmail.com>
2023-04-08 11:40:27 +0700

    add dependence mailservice

--- plate-number-service/app/settings.py
+++ plate-number-service/app/settings.py
@@ -2,10 +2,12 @@
 from app.service.detect_plate_service import DetectPlateService
 from app.service.user_service import UserService
 from model.implementation.user_implement import UserImplement
+from app.service.email_service import MailService
 
 #SERVICE
 user_service = UserService()
 PlateService = DetectPlateService
+mail_service = MailService()
 
 
 #MODEL
