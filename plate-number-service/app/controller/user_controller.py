e6d4253b51a36683995a0ddaf7693f376c465b35
thuhang2510 <tunguyet2510@gmail.com>
2023-04-10 17:19:50 +0700

    add annotation wrap_responses

--- plate-number-service/app/controller/user_controller.py
+++ plate-number-service/app/controller/user_controller.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from util.helper import wrap_responses
 from schemas import ResetPasswordSchema, UpdatePasswordSchema, UserSchema, UpdateUserSchema
 from app.settings import user_service
 from app.settings import user_model
@@ -8,34 +9,41 @@ router = APIRouter(
     tags=['users']
 )
 
+@router.put('/forget_password')
+@wrap_responses
+async def reset_password(resetPassword: ResetPasswordSchema):
+    resetPassword = resetPassword.dict()
+    return user_service.reset_password(resetPassword, user_model)
+
 @router.get('/')
-def get_all_user():
+@wrap_responses
+async def get_all_user():
     return user_service.get_all_user(user_model)
 
 @router.get('/{id}')
-def get_user_by_id(id: int):
-    return {'data': user_service.get_by_id(id, user_model)}
+@wrap_responses
+async def get_user_by_id(id: int):
+    return user_service.get_by_id(id, user_model)
 
 @router.post('')
+@wrap_responses
 async def create(user: UserSchema):
     user=user.dict()
-    return {'data': user_service.create(user, user_model)}
+    return user_service.create(user, user_model)
 
 @router.delete('/{id}')
+@wrap_responses
 async def delete(id: int):
-    return {'code': user_service.delete_by_id(id, user_model)}
+    return user_service.delete_by_id(id, user_model)
 
 @router.put('/{id}')
+@wrap_responses
 async def update(id: int, user: UpdateUserSchema):
     user = user.dict()
-    return {'data': user_service.update(id, user, user_model)}
-
-@router.put('/forget_password')
-async def reset_password(resetPassword: ResetPasswordSchema):
-    resetPassword = resetPassword.dict()
-    return {'data': user_service.reset_password(resetPassword, user_model)}
+    return user_service.update(id, user, user_model)
 
 @router.put('/{id}/updatePassword')
+@wrap_responses
 async def update_password(id: int, update_password: UpdatePasswordSchema):
     update_password = update_password.dict()
-    return {'data': user_service.update_password(id, update_password, user_model)}
+    return user_service.update_password(id, update_password, user_model)
