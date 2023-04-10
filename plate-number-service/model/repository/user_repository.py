ecafaf742ae95e6bbd797e96ae3dee800a4a5d98
thuhang2510 <tunguyet2510@gmail.com>
2023-04-10 17:18:59 +0700

    fix from return one to return three

--- plate-number-service/model/repository/user_repository.py
+++ plate-number-service/model/repository/user_repository.py
@@ -11,10 +11,14 @@ class UserRepository():
 
     def get_all_users(self, filter=None):
         try:
-            return self.db.query(DbUser).filter_by(**filter).all()
+            users = self.db.query(DbUser).filter_by(**filter).all()
+            if users is None:
+                return None, -1, "Get users fail"
+            
+            return users, 0, "Get users success"
         except Exception as e:
             logger.exception(e)
-            return None
+            return None, -1, "Get users fail"
     
     def create(self, data):
         try:
@@ -22,10 +26,10 @@ class UserRepository():
             self.db.commit()
             self.db.refresh(data)
             
-            return data
+            return data, 0, "Create user success"
         except Exception as e:
             logger.exception(e)
-            return None
+            return None, -1, "Create user fail"
         
     def get(self, id, filter=None):
         try:
@@ -36,17 +40,27 @@ class UserRepository():
 
             filter["id"] = id
             
-            return self.db.query(DbUser).filter_by(**filter).first()
+            user = self.db.query(DbUser).filter_by(**filter).first()
+
+            if user is None:
+                return None, -1, "Get user fail"
+            
+            return user, 0, "Get user success"
         except Exception as e:
             logger.exception(e)
-            return None
+            return None, -1, "Get user fail"
         
     def get_by(self, filter):
         try:
-            return self.db.query(DbUser).filter_by(**filter).first()
+            user = self.db.query(DbUser).filter_by(**filter).first()
+
+            if user is None:
+                return None, -1, "Get user fail"
+            
+            return user, 0, "Get user success"
         except Exception as e:
             logger.exception(e)
-            return None
+            return None, -1, "Get user fail"
         
     def get_by_and_update(self, filter, update):
         try:
@@ -54,10 +68,12 @@ class UserRepository():
             count = data.update(update)
             self.db.commit()
 
-            return count
+            if count == 1:
+                return count, 0, "Update success"
+            return None, -1, "Update fail"
         except Exception as e:
             logger.exception(e)
-            return None
+            return None, -1, "Update fail"
     
     def update(self, id, filter):
         try:
@@ -65,7 +81,9 @@ class UserRepository():
             count = data.update(filter)
             self.db.commit()
 
-            return count
+            if count == 1:
+                return count, 0, "Update success"
+            return None, -1, "Update fail"
         except Exception as e:
             logger.exception(e)
-            return -1
+            return None, -1, "Update fail"
