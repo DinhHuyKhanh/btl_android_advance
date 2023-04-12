d1a45654ec7d3f199085a4b36321d7299be390ba
thuhang2510 <tunguyet2510@gmail.com>
2023-04-12 11:09:23 +0700

    add dependence of GateService and GateHistoryModelImp

--- plate-number-service/app/settings.py
+++ plate-number-service/app/settings.py
@@ -2,13 +2,17 @@
 from app.service.detect_plate_service import DetectPlateService
 from app.service.user_service import UserDataService
 from model.implementation.user_implement import UserImplement
+from model.implementation.gate_history_implement import GateHistoryImplement
 from app.service.email_service import MailService
+from app.service.gate_history_service import GateHistoryService
 
 #SERVICE
 UserService = UserDataService
 PlateService = DetectPlateService
 mail_service = MailService()
+GateService = GateHistoryService
 
 
 #MODEL
 UserModelImp = UserImplement
+GateHistoryModelImp = GateHistoryImplement
