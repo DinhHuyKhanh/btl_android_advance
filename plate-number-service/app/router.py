4b171de0c690460dbd3ceb882e920807c3365fcc
thuhang2510 <tunguyet2510@gmail.com>
2023-04-12 11:03:24 +0700

    add router of gate_history_router

--- plate-number-service/app/router.py
+++ plate-number-service/app/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.controller.user_controller import router as user_router
 from app.controller.detect_plate_controller import router as plate_router
 from app.controller.email_controller import router as email_router
+from app.controller.gate_history_controller import router as gate_history_router
 
 api_router = APIRouter(
     prefix='/api/v1'
@@ -10,3 +11,4 @@ api_router = APIRouter(
 api_router.include_router(user_router)
 api_router.include_router(plate_router)
 api_router.include_router(email_router)
+api_router.include_router(gate_history_router)
